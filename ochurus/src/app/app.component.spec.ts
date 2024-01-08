551a3ade8612f24f69771093e696624d53644bd6
cnlund <91978441+cnlund@users.noreply.github.com>
2024-01-08 09:35:59 -0500

    angular intento 2

--- ochurus/src/app/app.component.spec.ts
+++ ochurus/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ochurus-websites' title`, () => {
+  it(`should have the 'ochurus' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ochurus-websites');
+    expect(app.title).toEqual('ochurus');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ochurus-websites');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ochurus');
   });
 });
