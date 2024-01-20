0421ddf96a140fc0379b11db41d218032cebb981
cnlund <91978441+cnlund@users.noreply.github.com>
2024-01-20 09:00:03 -0500

    prueba routing

---
+++ backend.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile("index/main.html", {
+        root: __dirname
+    });
+})
+
+app.get("/admin/cnlund/secreto/main/css", (req, res) => {
+    res.sendFile("index/index.css", {
+        root: __dirname
+    });
+})
+
+app.get("/admin/cnlund/secreto/main/img/fotomia", (req, res) => {
+    res.sendFile("index/fotomia.png", {
+        root: __dirname
+    });
+})
+
+app.listen(443)
