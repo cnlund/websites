dd6c9fb630d067bee9ab0c7eba0a74cf6c77606b
cnlund <91978441+cnlund@users.noreply.github.com>
2024-01-21 00:23:14 -0500

    adaptacion del enrutamiento

--- backend.js
+++ backend.js
@@ -20,6 +20,18 @@ app.get("/admin/cnlund/secreto/main/img/fotomia", (req, res) => {
     });
 })
 
+app.get("/blogs", (req, res) => {
+    res.sendFile("blog/blog.html", {
+        root: __dirname
+    });
+})
+
+app.get("/admin/cnlund/secreto/blog/css", (req, res) => {
+    res.sendFile("blog/blog.css", {
+        root: __dirname
+    });
+})
+
 app.listen(3000, () => {
     console.log("ejecutando el backend")
 })
